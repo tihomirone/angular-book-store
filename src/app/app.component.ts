f32a13c4a04e3675fb1d67f11de1a017864b952b
tihomirone <tihomir17@yahoo.com>
2025-01-28 12:27:29 +0200

    All components converted to standalone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './guest/login/login.component';
+import { HomeComponent } from './guest/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

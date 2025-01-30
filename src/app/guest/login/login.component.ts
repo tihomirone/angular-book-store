9deffd8e6c3441d4bad4fbb880bae1b275ee701f
tihomirone <tihomir17@yahoo.com>
2025-01-31 01:41:55 +0200

    Login page and styles added.

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { User } from '../../model/user.model';
+import { AuthenticationService } from '../../service/authentication.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FontAwesomeModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  user: User = new User();
+  faUser = faUserCircle;
+  errorMessage: string = "";
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {}
+
+  ngOnInit(): void {
+    if (this.authenticationService.currentUserValue?.id) {
+      this.router.navigate(['/profile']);
+      return;
+    }
+  }
+
+  login() {
+    this.authenticationService.login(this.user).subscribe(data => {
+      this.router.navigate(['/profile']);
+    }, err => {
+      this.errorMessage = "Username or password is incorrect!"
+      console.log(err);
+    });
+  }
 }

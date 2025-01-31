11720e9deaa623b7ccd53aafbbf9265301823d65
tihomirone <tihomir17@yahoo.com>
2025-01-31 02:06:41 +0200

    Page refinements. Added more control over the visibility of the links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { User } from './model/user.model';
+import { AuthenticationService } from './service/authentication.service';
+import { Role } from './model/role.enum';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,21 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-book-seller';
+
+  currentUser: User = new User();
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
+
+  isAdmin() {
+    return this.currentUser?.role === Role.ADMIN;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }

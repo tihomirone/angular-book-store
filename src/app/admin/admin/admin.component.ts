55b9f445810bdc4086257ad6c22269d2f96d44a2
tihomirone <tihomir17@yahoo.com>
2025-02-01 16:38:39 +0200

    Admin component and page

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Book } from '../../model/book.model';
+import { BookService } from '../../service/book.service';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+  bookList: Array<Book> = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.getAllBooks().subscribe(data => {
+      this.bookList = data;
+    })
+  }
 }

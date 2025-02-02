be53b39674347121a655899b7ebbaf771fd0783d
tihomirone <tihomir17@yahoo.com>
2025-02-02 21:57:04 +0200

    Book component modal popup.

--- src/app/admin/book/book.component.ts
+++ src/app/admin/book/book.component.ts
@@ -1,29 +1,33 @@
-import { Component } from '@angular/core';
-import { Book } from '../../model/book.model';
-import { BookService } from '../../service/book.service';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
+import {Book} from "../../model/book.model";
+import {BookService} from "../../service/book.service";
+import { FormsModule } from '@angular/forms';
 
 declare var $: any;
 
 @Component({
   selector: 'app-book',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './book.component.html',
-  styleUrl: './book.component.css'
+  styleUrls: ['./book.component.css']
 })
 export class BookComponent {
 
-  book: Book = new Book();
   errorMessage: string = "";
 
-  constructor(private bookService: BookService) {}
+  @Input() book: Book = new Book();
+  @Output() save = new EventEmitter<any>();
+  constructor(private bookService: BookService) { }
 
   saveBook() {
     this.bookService.saveBook(this.book).subscribe({
       next: (data) => {
-      }, error: (err) => {
-        this.errorMessage = 'Unexpected error occured!';
-        console.log(err);
-      }});
+      //this.save.emit(data);
+      $('#bookModal').modal('hide');
+    }, error: (err) => {
+      this.errorMessage = 'Unexpected error occurred.';
+      console.log(err);
+    }});
   }
 
   showBookModal() {

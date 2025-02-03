81316bff3dd8468c70e8008482e279c1228e969d
tihomirone <tihomir17@yahoo.com>
2025-02-03 13:33:41 +0200

    Add functionality for Edit book.

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -13,6 +13,7 @@ import { BookComponent } from "../book/book.component";
 export class AdminComponent {
 
   bookList: Array<Book> = [];
+  selectedBook: Book = new Book;
 
   @ViewChild(BookComponent) child: BookComponent | undefined;
   constructor(private bookService: BookService) {}
@@ -24,10 +25,21 @@ export class AdminComponent {
   }
 
   createBookRequest() {
+    this.selectedBook = new Book;
+    this.child?.showBookModal();
+  }
+
+  editBookRequest(item: Book) {
+    this.selectedBook = Object.assign({}, item);
     this.child?.showBookModal();
   }
 
   saveBookWatcher(book: Book) {
-    this.bookList.push(book);
+    let itemIndex = this.bookList.findIndex(item => item.id === book.id);
+    if (itemIndex !== -1) {
+      this.bookList[itemIndex] = book;
+    } else {
+      this.bookList.push(book);
+    }
   }
 }
